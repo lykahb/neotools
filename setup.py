c583287f762e573b786f59f898838e5c9906c621
Boris Lykah <lykahb@gmail.com>
2020-11-02 02:26:14 -0500

    Fix pypi errors

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='neotools',
-    description='Python USB access module',
+    description='Command-line tools for AlphaSmart NEO',
     author='Boris Lykah',
 )
